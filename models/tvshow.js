ec8239a7b5684936610d40baf105103905e5d043
Milton Bravo <miltonbravo96@gmail.com>
2019-01-24 23:51:08 -0600

    formato modificado

--- models/tvshow.js
+++ models/tvshow.js
@@ -2,15 +2,15 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var tvshowSchema = new Schema({
-    title:{type: String},
-    year:{type: Number},
-    country:{type: String},
-    poster:{type: String},
-    seasons:{type: Number},
-    genre:{type: String, enum:[
+    title:      { type: String },
+    year:       { type: Number },
+    country:    { type: String },
+    poster:     { type: String },
+    seasons:    { type: Number },
+    genre:      { type: String, enum:[
         'Drama', 'Fantasy', 'Sci-Fi', 'Thriller', 'Comedy'
     ]},
-    summary:{type: String}
+    summary:    { type: String }
 });
 
 module.exports = mongoose.model('TVShow', tvshowSchema);
