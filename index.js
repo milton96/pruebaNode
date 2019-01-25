4a95b724479bee8c4021858580ae6223c86180a6
Milton Bravo <miltonbravo96@gmail.com>
2019-01-24 22:57:31 -0600

    prueba servidor

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var mongoose = require("mongoose");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.send("Hola mundo");
+});
+
+app.use(router);
+
+app.listen(3000, function() {
+    console.log("Servidor Node en el puerto 3000");
+});
